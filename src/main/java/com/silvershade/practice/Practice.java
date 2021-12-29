866dfec6560c6d35f1ab90129b6890258e642167
Alex <shantemgardenix@gmail.com>
2021-12-29 13:39:37 +0500

    Added 2 blocks: titanium ore and titanium block

--- src/main/java/com/silvershade/practice/Practice.java
+++ src/main/java/com/silvershade/practice/Practice.java
@@ -1,5 +1,6 @@
 package com.silvershade.practice;
 
+import com.silvershade.practice.block.ModBlocks;
 import com.silvershade.practice.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -25,6 +26,7 @@ public class Practice
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
 
