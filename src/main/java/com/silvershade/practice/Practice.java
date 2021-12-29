aa4bb844471c438dc1f17f8772a3c298a46e9eb0
Alex <shantemgardenix@gmail.com>
2021-12-29 12:42:50 +0500

    Added 3 items: titanium ingot, titanium nugget and raw titanium

--- src/main/java/com/silvershade/practice/Practice.java
+++ src/main/java/com/silvershade/practice/Practice.java
@@ -1,22 +1,14 @@
-package com.example.examplemod;
+package com.silvershade.practice;
 
-import net.minecraft.world.level.block.Block;
+import com.silvershade.practice.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Practice.MOD_ID)
@@ -31,7 +23,11 @@ public class Practice
     public Practice() {
         // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
         eventBus.addListener(this::setup);
+
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
